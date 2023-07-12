b6f6c99fa1a1f30961221c06afd8e3e210f06dee
James Holbert <james.holbert@emsibg.com>
2023-07-12 10:55:11 -0700

    wip

--- app/routes/fish/index.tsx
+++ app/routes/fish/index.tsx
@@ -6,12 +6,19 @@ export async function loader() {
 }
 
 export default function Index() {
-  const fish = useLoaderData<typeof loader>();
+  const fishies = useLoaderData<typeof loader>();
 
-  fish.forEach((f) => console.log(f.name));
+  fishies.forEach((fish) => console.log(fish.name));
   return (
     <main className="">
       Main page for fishies
+      {fishies.map((fish) => (
+        <div>
+          <Link to={`/fish/${fish.id}`} key={fish.name}>
+            {fish.name}
+          </Link>
+        </div>
+      ))}
       <div>
         <Link to="/fish/new">Add Fish</Link>
       </div>
